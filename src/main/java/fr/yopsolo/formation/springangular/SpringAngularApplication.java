3e66b05f9e6bdbe3c82b2e2a51b9ed4dc78c8855
yann planchais <yann.planchais@randstaddigital.fr>
2024-12-17 23:42:57 +0100

    exposition des ID sur appels REST lors de l'utilisation de Spring DATA

--- src/main/java/fr/yopsolo/formation/springangular/SpringAngularApplication.java
+++ src/main/java/fr/yopsolo/formation/springangular/SpringAngularApplication.java
@@ -1,13 +1,30 @@
 package fr.yopsolo.formation.springangular;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+
+import fr.yopsolo.formation.springangular.entite.Produit;
 
 @SpringBootApplication
-public class SpringAngularApplication {
+public class SpringAngularApplication implements CommandLineRunner{
 
+	@Autowired
+	private RepositoryRestConfiguration repositoryRestConfiguration;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringAngularApplication.class, args);
+	}
+
+	/**
+	 * Permet d'exposer les ID des objets lors des appels REST
+	 */
+	@Override
+	public void run(String... args) throws Exception {
+		repositoryRestConfiguration.exposeIdsFor(Produit.class);
+		
 	}
 
 }
