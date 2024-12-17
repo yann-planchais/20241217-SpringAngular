73ee73b4d40f39e6dfab0a7e3e48d411c158dce3
yann planchais <yann.planchais@randstaddigital.fr>
2024-12-17 16:27:54 +0100

    creation projet

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # 20241217-SpringAngular
 Formation Spring Angular sur la plateforme UDEMI de Nadhem BELHADJ
+
+https://randstad.udemy.com/course/developpez-en-full-stack-avec-spring-boot-et-angular/learn/lecture/31815632#overview
+
+
---
+++ src/test/java/fr/yopsolo/formation/springangular/SpringAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.yopsolo.formation.springangular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAngularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
