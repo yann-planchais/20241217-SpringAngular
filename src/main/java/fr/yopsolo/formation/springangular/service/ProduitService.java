fee24e6716a1ceac2c162ab970bf9dd5af62f41c
yann planchais <yann.planchais@randstaddigital.fr>
2024-12-17 19:07:38 +0100

    ajout projet entier avec support bdd

---
+++ src/main/java/fr/yopsolo/formation/springangular/service/ProduitService.java
@@ -0,0 +1,25 @@
+package fr.yopsolo.formation.springangular.service;
+
+import java.util.List;
+
+import fr.yopsolo.formation.springangular.entite.Categorie;
+import fr.yopsolo.formation.springangular.entite.Produit;
+
+public interface ProduitService {
+	
+	 Produit saveProduit(Produit p);
+	 Produit updateProduit(Produit p);
+	 void deleteProduit(Produit p);
+     void deleteProduitById(Long id);
+	 Produit getProduit(Long id);
+	 List<Produit> getAllProduits();
+	 
+	 List<Produit> findByNomProduit(String nom);
+	 List<Produit> findByNomProduitContains(String nom);
+	 List<Produit> findByNomPrix (String nom, Double prix);
+	 List<Produit> findByCategorie (Categorie categorie);
+	 List<Produit> findByCategorieIdCat(Long id);
+	 List<Produit> findByOrderByNomProduitAsc();
+	 List<Produit> trierProduitsNomsPrix();
+
+}
