1f153295f593af0a97cfde586404fba596be43d8
yann planchais <yann.planchais@randstaddigital.fr>
2024-12-19 18:09:41 +0100

    ajout composant add-produit

---
+++ angular/MesProduits/src/app/add-produit/add-produit.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-produit',
+  imports: [],
+  templateUrl: './add-produit.component.html',
+  styleUrl: './add-produit.component.css'
+})
+export class AddProduitComponent {
+
+}
--- angular/MesProduits/src/app/app.routes.ts
+++ angular/MesProduits/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { ProduitsComponent } from './produits/produits.component';
+import { AddProduitComponent } from './add-produit/add-produit.component';
 
 
 export const routes: Routes = [
-   { path: "produits", component : ProduitsComponent }
+   { path: "produits", component : ProduitsComponent },
+   { path: "add-produit", component : AddProduitComponent }
 ];
