a0e6eb83c1a2e8af17855d769cfbf3c4ca27de7e
yann planchais <yann.planchais@randstaddigital.fr>
2024-12-19 18:12:58 +0100

    ajout redirection par défaut

--- angular/MesProduits/src/app/app.routes.ts
+++ angular/MesProduits/src/app/app.routes.ts
@@ -5,5 +5,7 @@ import { AddProduitComponent } from './add-produit/add-produit.component';
 
 export const routes: Routes = [
    { path: "produits", component : ProduitsComponent },
-   { path: "add-produit", component : AddProduitComponent }
+   { path: "add-produit", component : AddProduitComponent },
+   { path: "",  redirectTo:"produits", pathMatch:"full"},
+   
 ];
