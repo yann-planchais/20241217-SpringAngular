40093f5e5ac85ba245ddbd53893c26d99b02c1c0
yann planchais <yann.planchais@randstaddigital.fr>
2024-12-20 09:26:22 +0100

    option boucle @for

--- angular/MesProduits/src/app/produits/produits.component.ts
+++ angular/MesProduits/src/app/produits/produits.component.ts
@@ -1,17 +1,22 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Produit } from '../model/produit.model';
 
 @Component({
   selector: 'app-produits',
-  imports: [],
+  imports: [ CommonModule],
   templateUrl: './produits.component.html',
   styleUrl: './produits.component.css'
 })
 export class ProduitsComponent {
 
-  produits: string[]; // un tableau de chaines de caractères représentant la liste des produits
+  produits: Produit[]; // un tableau de produits
 
   constructor() {
-    this.produits = ["PC Asus", "Imprimante Epson", "Tablette samsung", "test"];
+    this.produits = [
+      {idProduit : 1,  nomProduit : "PC Asus", prixProduit : 3000.600, dateCreation : new Date("01/14/2011")},
+      {idProduit : 2,  nomProduit : "Imprimante Epson", prixProduit : 450, dateCreation : new Date("12/17/2010")},
+      {idProduit : 3,  nomProduit :"Tablette Samsung", prixProduit : 900.123, dateCreation : new Date("02/20/2020")}
+            ]; 
   }
 }
