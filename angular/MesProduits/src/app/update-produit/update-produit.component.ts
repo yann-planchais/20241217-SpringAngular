77a19aa4e7fd86937efaabefd121a6321697f74d
yann planchais <yann.planchais@randstaddigital.fr>
2025-01-07 17:04:16 +0100

    modif produits (raté sur commit)

--- angular/MesProduits/src/app/update-produit/update-produit.component.ts
+++ angular/MesProduits/src/app/update-produit/update-produit.component.ts
@@ -21,16 +21,31 @@ export class UpdateProduitComponent implements OnInit{
 
   }
   ngOnInit(): void {   
-    this.currentProduit = this.produitService.consulterProduit(this.activateRoute.snapshot.params['idProduit']);
+    this.produitService.consulterProduit(this.activateRoute.snapshot.params['idProduit']).subscribe( prod => {
+      this.currentProduit = prod;
+    });
    // this.categories = this.produitService.listeCategorie();
    // this.updatedCatId = this.currentProduit.categorie.idCat;
     console.log(this.currentProduit);
   }
 
 
-  modifierProduit() : void {
+  modifierProduit_oldTableau() : void {
   //  this.currentProduit.categorie = this.produitService.consulterCategorie(this.updatedCatId);
     this.produitService.miseAJourProduit(this.currentProduit);
-    this.router.navigate(['produits']);
+    this.router.navigate(['produits']); 
   }
+
+  modifierProduit() : void {
+    //  this.currentProduit.categorie = this.produitService.consulterCategorie(this.updatedCatId);
+
+      this.produitService.miseAJourProduit(this.currentProduit).subscribe(
+        {
+          next : prod => this.router.navigate(['produits']),
+          error : e => {alert('Erreur lors de la modification : ' + e)}        
+        }
+    );
+     
+    }
+
 }
